0b26be3fbce390b3618fc3e9b2fbc60b7cc7264f
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-07-17 12:20:28 +0800

    added: stats

--- src/routes/report.route.ts
+++ src/routes/report.route.ts
@@ -3,8 +3,11 @@ import {
   downloadTransactionReport,
   getTransactionReport,
 } from "../controllers/report.controller";
+import { authorize } from "../middleware/auth";
+import { ClientRole } from "../enums/roles";
 
 export const reportRouter = Router();
 
+reportRouter.use(authorize(ClientRole.USER));
 reportRouter.get("/", getTransactionReport);
 reportRouter.get("/download-transaction", downloadTransactionReport);
